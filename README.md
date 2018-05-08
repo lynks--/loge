236964f92be12d2d4ad39423aef8f0184cf60e42
Straylight <straylight@fractal>
2018-05-08 21:39:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+loge
+====
+
+**loge - simple, fast terminal logging for rust**
+
+_Overview_
+
+Just want simple terminal logging macros? Feel like [slog](https://docs.rs/slog) is overkill for your project and
+don't feel good about the runtime costs of [log](https://docs.rs/log)?
+
+_Features_
+
+- No run-time verbosity level checking, zero-cost macros in the disabled case.
+- Three simple levels: `debug!` (opt in), `info!` (opt out), and `warn!` (always on).
+
+_Usage_
+
+```rust
+debug!("about to do the thing...");	// only printed if enable the loge-debug feature
+if let Ok(done) = do_the_thing() {
+	info!("thing done:  {:?}", done); // printed unless you enable the loge-no-info feature
+}
+else {
+	warn!("failed to do the thing!"); // always printed
+}
+```
---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+// --- core loge macro, dont call this directly -----------------------------------------
+#[macro_export]
+macro_rules! loge {
+	($lvl: expr, $fmt: expr)				=> (
+		println!(concat!(
+			env!("CARGO_PKG_NAME"), " ", $lvl, " ({}) > ", $fmt
+		), module_path!())
+	);
+	($lvl: expr, $fmt: expr, $($args:tt)*)	=> (
+		println!(concat!(
+			env!("CARGO_PKG_NAME"), " ", $lvl, " ({}) > ", $fmt
+		), module_path!(), $($args)*)
+	);
+}
+
+// --- debug output (opt in with cfg loge_debug) ----------------------------------------
+#[cfg(feature = "loge-debug")]
+#[macro_export]
+macro_rules! debug {
+	($fmt: expr)					=> (loge!("dbug", $fmt));
+	($fmt: expr, $($args:tt)*)		=> (loge!("dbug", $fmt, $($args)*));
+}
+#[cfg(not(feature = "loge-debug"))]
+#[macro_export]
+macro_rules! debug {
+	($($_: tt)*) => { };
+}
+
+// --- info output (opt out with cfg no_loge_info) --------------------------------------
+#[macro_export]
+#[cfg(not(feature = "no-loge-info"))]
+macro_rules! info {
+	($fmt: expr)					=> (loge!("info", $fmt));
+	($fmt: expr, $($args:tt)*)		=> (loge!("info", $fmt, $($args)*));
+}
+
+#[macro_export]
+#[cfg(feature = "no-loge-info")]
+macro_rules! info {
+	($($_: tt)*) => { };
+}
+
+// --- warn output (always on) ----------------------------------------------------------
+#[macro_export]
+macro_rules! warn {
+	($fmt: expr)					=> (loge!("WARN", $fmt));
+	($fmt: expr, $($args:tt)*)		=> (loge!("WARN", $fmt, $($args)*));
+}
+
+// --- tests ----------------------------------------------------------------------------
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn loge() {
+		debug!("wow, such debug");
+
+		let s = String::from("so doge");
+		info!("the string {:?} is {} characters long", s, s.len());
+
+		warn!("much warning");
+    }
+}
